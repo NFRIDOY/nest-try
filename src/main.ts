f6c99cf3ae912eacb01dd2968804ecf4baeb8d99
nfridoy <nfridoy@gmail.com>
2026-05-29 05:34:54 +0600

    🔧 config: swagger setup: one click setup 🔥

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  // Configure Swagger options
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Try API')
+    .setDescription('Interactive API documentation')
+    .setVersion('1.0')
+    .addTag('NestJs Try')
+    .build();
+
+  // Create the Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup the UI at http://localhost:3000/api
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
